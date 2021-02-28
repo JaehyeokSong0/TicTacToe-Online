2449ec2d05dbb92fdc247a0b5b402e38162e629b
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-03-01 00:32:24 +0900

    apply localhost environment & improve project structure

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
-const port = 8080
+const port = 3000
 
-app.get('/',(req,res)=>res.send("Hello world!"))
+app.set('view engine','ejs')
+app.engine('html',ejs.renderFile)
 
-app.listen(port,() => console.log(`app listening on port ${port}.`))
+app.use(express.static('public'))
+
+app.get('', function(req,res){
+    res.render('index.html')
+});
+
+var server = app.listen(port,() => 
+    console.log(`app listening on port ${port}.`)
+)
