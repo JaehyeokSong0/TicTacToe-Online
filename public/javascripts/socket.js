bfbd166a9eebb2b0056f9dabecde754d0dc24b77
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-06-29 00:43:43 +0900

    Fix deprecated variable name

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,4 +1,4 @@
-const name = prompt('이름을 입력하세요');
+const id = prompt('이름을 입력하세요');
 const socket = io();
 var room = ['room1', 'room2'];
 var num = 0;
@@ -10,34 +10,34 @@ var autoScroll = function () {
     }, 0);
 };
 
-socket.emit('joinRoom', num, name);
+socket.emit('joinRoom', num, id);
 
 $('select').change(() => {
-    socket.emit('leaveRoom', num, name);
+    socket.emit('leaveRoom', num, id);
     num++;
     num = num % 2; //본격적인 구현 시 로직 수정 필요
-    socket.emit('joinRoom', num, name);
+    socket.emit('joinRoom', num, id);
 });
 
 $('#chat').submit((e) => {
     e.preventDefault();
-    socket.emit('sendMsg', num, name, $('#chatBox').val());
+    socket.emit('sendMsg', num, id, $('#chatBox').val());
     $('#chatBox').val("");
 });
 
-socket.on('sendMsg', (name, msg) => {
-    $('#messages').append($('<li>').text(name + " : " + msg));
+socket.on('sendMsg', (id, msg) => {
+    $('#messages').append($('<li>').text(id + " : " + msg));
     autoScroll();
 });
 
-socket.on('joinRoom', (num, name) => {
-    $('#messages').append($('<li>').text("[System] " + name + ' joined ' + room[num]));
+socket.on('joinRoom', (num, id) => {
+    $('#messages').append($('<li>').text("[System] " + id + ' joined ' + room[num]));
     autoScroll();
 
 });
 
-socket.on('leaveRoom', (num, name) => {
-    $('#messages').append($('<li>').text("[System] " + name + ' leaved ' + room[num]));
+socket.on('leaveRoom', (num, id) => {
+    $('#messages').append($('<li>').text("[System] " + id + ' leaved ' + room[num]));
     autoScroll();
     $('#startBtn').attr('disabled', true);
 });
